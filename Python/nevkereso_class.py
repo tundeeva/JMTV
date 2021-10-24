4c128fd1ff69cd1ccaafb93189aa441dbce21d47
BudaViktor <vicktor0095@gmail.com>
2021-10-24 17:56:25 +0200

    nevkereso_class.py bővítése almenüvel

--- Python/nevkereso_class.py
+++ Python/nevkereso_class.py
@@ -1,2 +1,31 @@
+
+
 class Search:
-    def nevek(self):
+    '''Fő osztály'''
+    
+    def menu(label):
+        print(label)
+        
+        '''Egy kicsi menü, amiben választani lehet majd egy pár opció közül'''
+        
+        m_1 = "\n\tNév keresése a kiválasztot listából" + "\t--\t1"
+        print(m_1)
+        m_2 = "\tNév keresése az összes listából\t" + "\t--\t2"
+        print(m_2)
+        txt = "\n\tKérlek válasz az opciók közül: "
+        a = int(input(txt))
+        
+        # Ha függvény a választás megvalósításához
+        
+        if a == 1:
+            txt = "Sikeres keresés"
+            print(txt)
+        elif a == 2:
+            txt = "Sikeres keresés"
+            print(txt)
+            
+        
+# A modul inulásának parancsa
+Search.menu("\nÜdvözöllek a Névkeresőben!")
+# A modul teszteléséhez szükséges
+input("<<Enter>>")
