f4d9225114cec8e959b29fee54ff9a51f7b40418
BudaViktor <vicktor0095@gmail.com>
2021-10-26 22:06:23 +0200

    nevkereso_class.py bővítése, festmeny_v.txt hiba javítás

--- Python/nevkereso_class.py
+++ Python/nevkereso_class.py
@@ -1,31 +1,45 @@
 
-
-class Search:
+class InputFile:
+    def __init__(self, file, lista):
+        f = open(file, "r")
+        for sor in f:
+            sor = sor[:-1].split(";")
+            lista.append([str(sor[0]), int(sor[1]), str(sor[2])])
+        f.close()
+        return
+        
+class Search_cmd:
     '''Fő osztály'''
+    festmeny_now = []   #most üres de később ez lesz a pillanatnyi!!
+    festmeny_all = []   #összes eddigi festmeny.txt
     
-    def menu(label):
+    #Search_cmd.inputFile("festmeny_v.txt", festmeny_all)
+    
+    def __init__(self, label):
         print(label)
         
         '''Egy kicsi menü, amiben választani lehet majd egy pár opció közül'''
         
-        m_1 = "\n\tNév keresése a kiválasztot listából" + "\t--\t1"
-        print(m_1)
-        m_2 = "\tNév keresése az összes listából\t" + "\t--\t2"
-        print(m_2)
+        self.m_1 = "\n\tNév keresése a kiválasztot listából" + "\t--\t1"
+        print(self.m_1)
+        self.m_2 = "\tNév keresése az összes listából\t" + "\t--\t2"
+        print(self.m_2)
         txt = "\n\tKérlek válasz az opciók közül: "
-        a = int(input(txt))
+        self.a = int(input(txt))
         
         # Ha függvény a választás megvalósításához
         
-        if a == 1:
-            txt = "Sikeres keresés"
-            print(txt)
-        elif a == 2:
+        if self.a == 1:
+            for i in range(len(self.festmeny_all)):
+                print(self.festmeny_all[i][0])
+                
+        elif self.a == 2:
             txt = "Sikeres keresés"
             print(txt)
             
         
-# A modul inulásának parancsa
-Search.menu("\nÜdvözöllek a Névkeresőben!")
+# A modul inulásának parancsai
+InputFile("festmeny_v.txt", Search_cmd.festmeny_all)
+Search_cmd("\nÜdvözöllek a Névkeresőben!")
 # A modul teszteléséhez szükséges
 input("<<Enter>>")
