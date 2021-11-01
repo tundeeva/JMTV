29b4fa9dfdb992f13163bfb4cfd77239cafaf695
BenkeM <benke.mariaa@gmail.com>
2021-11-01 21:01:08 +0100

    csinos version

--- Python/fajlbeolvasas.py
+++ Python/fajlbeolvasas.py
@@ -2,25 +2,23 @@ import festmenyek_class
 
 class Beolvasas:
 
-    def __init__(self, rekordok):
-        self.rekordok = rekordok
-        fajl = input("Kérem a fájl nevét: ")
-        self.inputFile(fajl, self.rekordok)
+    def __init__(self, lista):
+        self.lista = lista
+        self.fajl = input("Kérem a fájl nevét .txt alakban: ")
+        self.inputFile()
         print("\tA fájl beolvasása ... kész!")
-
+        input("<< Enter >>")
         return
         
     
 
-    def inputFile(self, file, lista):
-        f = open(file, "r", encoding='utf-8')
-
-        self.rekordok[:] = []
-    
+    def inputFile(self):
+        f = open(self.fajl, "r", encoding='utf-8')
+        self.lista[:] = []
         for sor in f:
             sor=sor[:-1].split(";")
             peldany = festmenyek_class.Festmenyek(sor[0],sor[1],sor[2])
-            lista.append(peldany)
+            self.lista.append(peldany)
         f.close()
         return
 
