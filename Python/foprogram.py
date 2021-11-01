4cbfea798fb3ab257d3af155dc5f70e8de39a25b
janos1109 <janos1109@gmail.com>
2021-11-01 22:49:07 +0100

    kisebb módosítások a menürendszeren

--- Python/foprogram.py
+++ Python/foprogram.py
@@ -4,18 +4,37 @@ import fajlbeolvasas
 
 rekordok = []
 
-def foprogram():
+def main():
     udvozles_class.Udvozles()
     answer = ""
     while 1:
         fomenu.Fomenu()
         prompt = ">"
-        while answer != ('a' or 'b' or 'c' or 'd' or 'e' or 'f' or 'exit'):
+        while answer != 'exit':
             answer = input(prompt)
-        if answer == 'a':
-            fajlbeolvasas.Beolvasas(rekordok)
+            if answer == 'a':
+                fajlbeolvasas.Beolvasas(rekordok)
+                break
+            elif answer == 'b':
+                print("bbbb")
+                break
+            elif answer == 'c':
+                print("cccc")
+                break
+            elif answer == 'd':
+                print("dddd")
+                break
+            elif answer == 'e':
+                print("eeee")
+                break
+            elif answer == 'f':
+                print("ffff")
+                break
+            elif answer == 'exit':
+                quit()
+            answer = ""
     
 
 
 
-foprogram()
+main()
