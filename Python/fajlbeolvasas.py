12e6ad18ce7c45d3e6ee617d84bfc3f19630022a
BenkeM <benke.mariaa@gmail.com>
2021-11-01 19:45:30 +0100

    próbaverzió

--- Python/fajlbeolvasas.py
+++ Python/fajlbeolvasas.py
@@ -1,27 +1,29 @@
 import festmenyek_class
-rekordok = []
 
-def f1(label):
-    print(label)
+class Beolvasas:
+
+def __init__(self):
+    
     fajl = input("Kérem a fájl nevét: ")
     inputFile(fajl, rekordok)
     print("\tA fájl beolvasása ... kész!")
 
     for i in range(len(rekordok)):
         print("\t"+rekordok[i].toString())
+return
         
     
 
-def inputFile(file, lista):
-    f = open(file, "r", encoding='utf-8')
+def inputFile(self, file, lista):
+f = open(file, "r", encoding='utf-8')
 
-    rekordok[:] = []
+rekordok[:] = []
     
-    for sor in f:
-        sor=sor[:-1].split(";")
-        peldany = festmenyek_class.Festmenyek(sor[0],sor[1],sor[2])
-        lista.append(peldany)
-    f.close()
-    return
+for sor in f:
+    sor=sor[:-1].split(";")
+    peldany = festmenyek_class.Festmenyek(sor[0],sor[1],sor[2])
+    lista.append(peldany)
+f.close()
+return
 
-f1("Festmény adatbázis")
+
