e0b2992e04e9fa5bab23ebd81f344d82e8896c12
BenkeM <benke.mariaa@gmail.com>
2021-11-02 12:19:41 +0100

    Kiíró osztályka

---
+++ Python/fajl_kiiras.py
@@ -0,0 +1,13 @@
+import festmenyek_class
+class Kiiras:
+    def __init__(self, lista)
+   
+    print("\tA fájl adatai a következők: ")
+
+    for i in range(len(lista)):
+          print(lista[i].toString())
+
+    print("\tA főmenübe való visszalépéshez kérem nyomja meg az Entert”)
+    input("<<Enter>>")
+    return
+
